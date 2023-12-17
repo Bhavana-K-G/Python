cbd00bbee113b722392939f826862940af6ccdec
Bhavana-K-G <bhavanagowda768@gmail.com>
2023-12-17 10:41:06 +0530

    python

---
+++ assignment-1/conditional - Copy.py
@@ -0,0 +1,13 @@
+#1)program to read 2 numbers from the keyboard and print sum
+
+#2)read multiple values from the keyboard in a single line
+
+#3)check if a number is divisible by 7 or not
+
+#4)check if a number is multiple by 3 or not
+
+#5)check if a number is positive or not
+
+#6)
+
+
